88eee9dc0ef427d4f988f7e584517cbefa43d763
tokisami <jose.jacialr@gmail.com>
2026-07-15 07:25:32 -0400

    algo de limpieza de esto antes de dormir owo

--- pomodoro.py
+++ pomodoro.py
@@ -5,9 +5,10 @@
 # El minimo sera de 25 min y 5 min de descanso solamente, pero se podra tener 4 sesiones de 25 con 3 breaks de 5 minutos y 15 minutos
 # de ultimo break
 
-import time
 import tkinter as tk
 from tkinter import font
+import time
+import pygame
 
 #Display de la ventana 
 root = tk.Tk()
@@ -28,10 +29,11 @@ etiqueta = tk.Label(root, text="Pomodoro Goat", font=fuente_minecraft, fg="black
 etiqueta.pack(pady=35)
 
 #Vamos a intentar colocar ahora las imagenes de pausa y continuar de al medio de de la pantalla y dejare comentado el de continuar mientras
-pausa = tk.PhotoImage(file="pause.png")
-#continuar = tk.PhotoImage(file="continue.png") 
+idle = tk.PhotoImage(file="RalseiIDLE.gif")
+#break = tk.PhotoImage(file="RalseiBreak.png") 
+#focusing = tk.PhotoImage(file="RalseiFocusing.gif")
 
-etiqueta = tk.Label(root, image=pausa)
+etiqueta = tk.Label(root, image=idle, bg="#b1fcbb")
 etiqueta.pack()
 
 #En esta parte siguiendo el orden de la app voy a colocar el temporizador en pantalla y lo voy a centrar
@@ -56,20 +58,27 @@ botonReset.place(x=210, y=300, width=60, height=30)
 
 #Definir cantidad de ciclos de estudio (entre 1 y 4)
 def ciclosSeleccionados():
+    verify = 0
     if ciclos.get() == "1":
-        print("Ciclos seleccionados: 1")
+        print("1")
+        contadorPrint.config(text="1")
     elif ciclos.get() == "2":
-        print("Ciclos seleccionados: 2")
+        print("2")
+        contadorPrint.config(text="2")
     elif ciclos.get() == "3":
-        print("Ciclos seleccionados: 3")
+        print("3")
+        contadorPrint.config(text="3")
     elif ciclos.get() == "4":
-        print("Ciclos seleccionados: 4")
-
+        print("4")
+        contadorPrint.config(text="4")
+    
 ciclos = tk.StringVar(value="1") 
 tk.Radiobutton(root, command=ciclosSeleccionados, text="1", variable=ciclos, value="1", font=("Minecraft Default", 11), bg="#ff96f6").place(x=50, y=345, width=50, height=25)
 tk.Radiobutton(root, command=ciclosSeleccionados, text="2", variable=ciclos, value="2", font=("Minecraft Default", 11), bg="#ff96f6").place(x=100, y=345, width=50, height=25)
 tk.Radiobutton(root, command=ciclosSeleccionados, text="3", variable=ciclos, value="3", font=("Minecraft Default", 11), bg="#ff96f6").place(x=150, y=345, width=50, height=25)
 tk.Radiobutton(root, command=ciclosSeleccionados, text="4", variable=ciclos, value="4", font=("Minecraft Default", 11), bg="#ff96f6").place(x=200, y=345, width=50, height=25)
 
+contadorPrint = tk.Label(root, text="1", font=("Minecraft Default", 30), fg="black", bg="#b1fcbb")
+contadorPrint.place(x=240, y=110, width=30, height=30)
 
 root.mainloop()
